26b5d3636df074198db509c17b7981804b6ec067
Никитин Владимир Владимирович <nikitinvv.22@sch239.net>
2021-02-18 20:10:11 +0300

    Реализация прыжка завершена

--- CharacterControllerScript.cs
+++ CharacterControllerScript.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +6,7 @@ public class CharacterControllerScript : MonoBehaviour
 {
     private Animator anim;
     Rigidbody2D rb;
+    public Vector2 moveVector;
     public Transform groundCheck;
     public LayerMask whatIsGround;
     public float maxSpeed = 10f; 
@@ -14,6 +14,7 @@ public class CharacterControllerScript : MonoBehaviour
     private bool isFacingRight = true;
     float speedX; 
     private bool isGrounded = false;
+    //private bool Performed_Jump = false;
     private float groundRadius = 0.2f;
 
 	private void Start()
@@ -33,14 +34,13 @@ public class CharacterControllerScript : MonoBehaviour
             return;
         }
         
-        float move = Input.GetAxis("Horizontal");
+        float move = Input.GetAxis("Horizontal");//GetAxis - полчить ось, по которой передвигаем объект
+        anim.SetFloat("Speed", Mathf.Abs(move));
 
         rb.velocity = new Vector2(move * maxSpeed, rb.velocity.y);
 
-        if(move > 0 && !isFacingRight)
-            Flip();
-        else if (move < 0 && isFacingRight)
-            Flip();
+        moveVector.x = Input.GetAxis("Horizontal");
+        Reflect();
 
         transform.Translate(speedX, 0, 0);
         speedX = 0;
@@ -52,86 +52,26 @@ public class CharacterControllerScript : MonoBehaviour
         {
             anim.SetBool("Ground", false);
             rb.AddForce(new Vector2(0, verticalImpulse), ForceMode2D.Impulse);
+           // Perform();
         }
     }
-
-    private void Flip() //you have to change that
-    {
-        isFacingRight = !isFacingRight;
-
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-
-        transform.localScale = theScale;
-    }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CharacterControllerScript : MonoBehaviour
-{
-    public float maxSpeed = 10f; 
-    private bool isFacingRight = true;
-    private Animator anim;
-    Rigidbody2D rb;
-    float speedX; 
-    bool isGrounded;
-    public float verticalImpulse; 
-
-
-	private void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-    }
-
-	private void FixedUpdate()
-    {
-        float move = Input.GetAxis("Horizontal");
-
-        anim.SetFloat("Speed", Mathf.Abs(move));
-        
-        rb.velocity = new Vector2(move * maxSpeed, rb.velocity.y);
-
-        if(move > 0 && !isFacingRight)
-            Flip();
-        else if (move < 0 && isFacingRight)
-            Flip();
     
-
-      /* anim.Play("Idle", 0, 0.25f);
-        if (Input.GetKey(KeyCode.A)) {
-            speedX = -horizontalSpeed;
-            anim.Play("Run", 0, 0.25f);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            speedX = horizontalSpeed;
-            anim.Play("Run", 0, 0.25f);
-        }*/
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.AddForce(new Vector2(0, verticalImpulse), ForceMode2D.Impulse);
-        }
-        transform.Translate(speedX, 0, 0);
-        speedX = 0;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    void Reflect()
     {
-        Debug.Log(1);
-        if (collision.gameObject.tag == "Ground")
-            isGrounded = true;
+        if ((moveVector.x > 0 && !isFacingRight) || (moveVector.x < 0 && isFacingRight))
+        {
+            transform.localScale *= new Vector2(-1, 1);
+            isFacingRight = !isFacingRight;//меняем флажок на правильное направление
+        }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    /*private void Perform()
     {
-        if (collision.gameObject.tag == "Ground")
-            isGrounded = false;
-    }
+        anim.SetBool("Performed_Jump", false);
 
-    private void Flip()
+    }*/
+
+    /*private void Flip() //you have to change that
     {
         isFacingRight = !isFacingRight;
 
@@ -139,6 +79,5 @@ public class CharacterControllerScript : MonoBehaviour
         theScale.x *= -1;
 
         transform.localScale = theScale;
-    }
->>>>>>> d819e5d8c054a4da05865be4a21d39afc5f39621
+    }*/
 }
